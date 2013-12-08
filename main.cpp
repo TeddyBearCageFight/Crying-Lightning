6b66589944e9974c5fccf9b9b9ae5a95ad1fc527
Brennan Riddell <theawesometimetraveler@gmail.com>
2013-12-07 21:20:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <SOIL/SOIL.h>
 
 using namespace glm;
@@ -19,15 +21,18 @@ using namespace glm;
 // Shader sources
 const GLchar* vertexSource =
     "#version 150 core\n"
-    "in vec2 position;"
+    "in vec3 position;"
     "in vec3 color;"
     "in vec2 texcoord;"
     "out vec3 Color;"
     "out vec2 Texcoord;"
+    "uniform mat4 model;"
+    "uniform mat4 view;"
+    "uniform mat4 proj;"
     "void main() {"
     "   Color = color;"
     "   Texcoord = texcoord;"
-    "   gl_Position = vec4(position, 0.0, 1.0);"
+    "   gl_Position = proj * view * model * vec4(position, 1.0);"
     "}";
 const GLchar* fragmentSource =
     "#version 150 core\n"
@@ -37,23 +42,59 @@ const GLchar* fragmentSource =
     "uniform sampler2D texKitten;"
     "uniform sampler2D texPuppy;"
     "void main() {"
-    "   vec4 colKitten = texture( texKitten, Texcoord );"
-    "   vec4 colPuppy = texture( texPuppy, Texcoord );"
-    "   outColor = mix( colKitten, colPuppy, 0.5 );"
+    "   outColor = vec4(Color, 1.0) * mix(texture(texKitten, Texcoord), texture(texPuppy, Texcoord), 0.5);"
     "}";
 
-float vertices[] = {
-//  Position      Color             Texcoords
-    -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top-left
-     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top-right
-     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Bottom-right
-    -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  // Bottom-left
-};
+GLfloat vertices[] = {
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
 
-float pixels[] = {
-    0.0f, 0.0f, 0.0f,   1.0f, 1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,   0.0f, 0.0f, 0.0f
-};
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+
+        -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+         1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+         1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+         1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        -1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f
+    };
 
 int main(int argc, char* argv[])
 {
@@ -75,38 +116,30 @@ int main(int argc, char* argv[])
     glewExperimental = GL_TRUE;
     glewInit();
 
-    GLuint vao; //Vertex Array Object
+    // Initialize OpenGL
+    glEnable( GL_DEPTH_TEST );
+
+    // Create Vertex Array Object
+    GLuint vao; 
     glGenVertexArrays( 1, &vao );
     glBindVertexArray( vao );
 
-    GLuint vbo; //Vertex Buffer Object
+    // Create Vertex Array Object and copy the vertex data to it
+    GLuint vbo;
     glGenBuffers( 1, &vbo ); //Generate 1 buffer
+       
     glBindBuffer( GL_ARRAY_BUFFER, vbo );
     glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW ); 
-    
-    //Create an array element
-    GLuint ebo;
-    glGenBuffers( 1, &ebo );
 
-    GLuint elements[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-
-    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, ebo );
-    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW );
-
-    // Create and compile the Vertex Shader object
+    // Create and compile the Vertex Shader and Fragment Shader
     GLuint vertexShader = glCreateShader( GL_VERTEX_SHADER );
     glShaderSource( vertexShader, 1, &vertexSource, NULL );
     glCompileShader( vertexShader );
-
-    // Create and compile the Fragment Shader object
     GLuint fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
     glShaderSource( fragmentShader, 1, &fragmentSource, NULL );
     glCompileShader( fragmentShader );
     
-    // Combine the shader into a program
+    // Combine the shaders into a shader program
     GLuint shaderProgram = glCreateProgram();
     glAttachShader( shaderProgram, vertexShader );
     glAttachShader( shaderProgram, fragmentShader );
@@ -114,50 +147,92 @@ int main(int argc, char* argv[])
     glLinkProgram( shaderProgram );
     glUseProgram( shaderProgram );
 
-    // Retrieve a reference for the input in the vertex shader - first location will always be 0
+    // Specify the layout of the vertex data
     GLint posAttrib = glGetAttribLocation( shaderProgram, "position" );
     glEnableVertexAttribArray( posAttrib ); //Enable the vertex attrib
-    glVertexAttribPointer( posAttrib, 2, GL_FLOAT, GL_FALSE, 7*sizeof(float), 0 ); 
+    glVertexAttribPointer( posAttrib, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), 0 ); 
 
     GLint colAttrib = glGetAttribLocation( shaderProgram, "color" );
     glEnableVertexAttribArray( colAttrib );
-    glVertexAttribPointer( colAttrib, 3, GL_FLOAT, GL_FALSE, 7*sizeof(float), (void*)(2*sizeof(float)));
+    glVertexAttribPointer( colAttrib, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(2*sizeof(float)));
 
     GLint texAttrib = glGetAttribLocation( shaderProgram, "texcoord" );
     glEnableVertexAttribArray( texAttrib );
-    glVertexAttribPointer( texAttrib, 2, GL_FLOAT, GL_FALSE, 7*sizeof(float), (void*)(5*sizeof(float)));
+    glVertexAttribPointer( texAttrib, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(5*sizeof(float)));
 
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
     float color[] = { 1.0f, 0.0f, 0.0f, 1.0f };
     glTexParameterfv( GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, color );
     glGenerateMipmap( GL_TEXTURE_2D );
 
+    // Load textures
     GLuint textures[2];
     glGenTextures( 2, textures );
 
     int width, height;
     unsigned char* image;
 
-    glActiveTexture( GL_TEXTURE0 );
-    glBindTexture( GL_TEXTURE_2D, textures[0] );
-    image = SOIL_load_image("kitten.png", &width, &height, 0, SOIL_LOAD_RGB);
-    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image );
-    SOIL_free_image_data( image );
-    glUniform1i( glGetUniformLocation( shaderProgram, "texKitten"), 0 );
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+        image = SOIL_load_image("kitten.png", &width, &height, 0, SOIL_LOAD_RGB);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+        SOIL_free_image_data(image);
+    glUniform1i(glGetUniformLocation(shaderProgram, "texKitten"), 0);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+        image = SOIL_load_image("puppy.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+        SOIL_free_image_data(image);
+    glUniform1i(glGetUniformLocation(shaderProgram, "texPuppy"), 1);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     GLuint uniColor = glGetUniformLocation( shaderProgram, "triangleColor" );
 
-    
+    //Set up projection 
+    mat4 view = lookAt(
+        vec3( 1.2f, 1.2f, 1.2f),
+        vec3( 0.0f, 0.0f, 0.0f),
+        vec3( 0.0f, 0.0f, 1.0f)
+        );
+
+    GLint uniView = glGetUniformLocation( shaderProgram, "view" );
+    glUniformMatrix4fv( uniView, 1, GL_FALSE, value_ptr( view ));
+
+    mat4 proj = perspective( 45.0f, 800.0f / 600.0f, 1.0f, 10.0f );
+    GLint uniProj = glGetUniformLocation( shaderProgram, "proj" );
+    glUniformMatrix4fv( uniProj, 1, GL_FALSE, value_ptr( proj ));
+
     while ( glfwGetKey( window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && !glfwWindowShouldClose(window) ) {              
+        
+        // Clear the screen to black
         glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
-        glClear( GL_COLOR_BUFFER_BIT );
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
     
-        //glDrawArrays( GL_TRIANGLES, 0, 3 );
-        glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+        //Calculate transformation
+        mat4 model;
+        model = rotate( 
+            model, 
+            (float)clock() / (float)CLOCKS_PER_SEC * 180.0f, 
+            vec3( 0.0f, 0.0f, 1.0f )
+            );
+        vec4 result = model * vec4( 1.0f, 0.0f, 0.0f, 1.0f );
+        printf( "%f, %f, %f\n", result.x, result.y, result.z );
+
+        GLint uniModel = glGetUniformLocation( shaderProgram, "model" );
+        glUniformMatrix4fv( uniModel, 1, GL_FALSE, value_ptr( model ));
+
+        //Draw cube
+        glDrawArrays( GL_TRIANGLES, 0, 36 );
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
@@ -165,12 +240,11 @@ int main(int argc, char* argv[])
         // Poll for and process events
         glfwPollEvents();
     }
-    // Loop until the user closes the window w/ ESC
 
+    glDeleteTextures( 2, textures );
     glDeleteProgram( shaderProgram );
     glDeleteShader( fragmentShader );
     glDeleteShader( vertexShader );
-
 
     glDeleteBuffers( 1, &vbo );
 
